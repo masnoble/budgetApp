3298cfabe0f72b6edb834d04a330366e6c940cea
Joshua Thomas <jnthomas522@gmail.com>
2024-01-03 20:18:54 -0500

    Fix caching issue

--- webApp/src/app/page.tsx
+++ webApp/src/app/page.tsx
@@ -3,23 +3,26 @@ import TransactionEntryForm from "@ClientComponents/TransactionEntryForm";
 import { TransactionEntryOptions } from "@/lib/types";
 import { Box, Grid, Stack } from "@mui/material";
 import { MonthCode, Person } from "@prisma/client";
+import { unstable_noStore } from "next/cache";
 
 export default async function Page() {
   // TODO: use some kind of google auth and store this email in session.
 
-  const ignore = await initialSetup("jnthomas522@gmail.com")
+  unstable_noStore();
+
+  const ignore = await initialSetup("jnthomas522@gmail.com");
 
   const personAndFamily = await getPersonByEmail("jnthomas522@gmail.com");
-  const person: Person = { 
-    id: personAndFamily? personAndFamily.id : "",
-    email: personAndFamily? personAndFamily.email : "",
-    name: personAndFamily? personAndFamily.name: "",
-    familyId: personAndFamily? personAndFamily.familyId: ""
-  }
+  const person: Person = {
+    id: personAndFamily ? personAndFamily.id : "",
+    email: personAndFamily ? personAndFamily.email : "",
+    name: personAndFamily ? personAndFamily.name : "",
+    familyId: personAndFamily ? personAndFamily.familyId : "",
+  };
 
-  const family = personAndFamily!.family
+  const family = personAndFamily!.family;
   // const family = person!.family;
- 
+
   const transactionEntryOptions: TransactionEntryOptions = {
     currentUser: person,
     people: family.people,
@@ -30,13 +33,9 @@ export default async function Page() {
 
   return (
     <Box display={"flex"} flexGrow={1} alignItems={"center"}>
-    <Grid
-      container
-      justifyContent="center"
-      alignItems="stretch"
-      > 
-      <TransactionEntryForm passedOptions={transactionEntryOptions} />
-    </Grid>
-      </Box>
+      <Grid container justifyContent="center" alignItems="stretch">
+        <TransactionEntryForm passedOptions={transactionEntryOptions} />
+      </Grid>
+    </Box>
   );
 }
